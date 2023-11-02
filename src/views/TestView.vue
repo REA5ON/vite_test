0afd74c291289c61adf41508babbc18d1a51fb1b
REA5ON <valten007@gmail.com>
2023-11-02 11:34:57 +0100

    Initial commit

---
+++ src/views/TestView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="test">
+      <h1>This is a test page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .test {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
