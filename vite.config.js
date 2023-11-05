294a70499952352744347d0edc0bf1d7536ec95e
REA5ON <valten007@gmail.com>
2023-11-05 13:00:10 +0100

    build app

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/vite_test',
+  base: "/vite_test/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
